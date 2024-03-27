22972b35d3e4c33d2d47ca78e72489d2da228ecc
alexhalatiki <alexhvicente1@gmail.com>
2024-03-26 21:59:41 -0300

    Refatoracoes

--- formigas/src/formiga.rs
+++ formigas/src/formiga.rs
@@ -1,26 +1,17 @@
-use crate::Matriz;
-use rand::Rng;
-use std::cell::RefCell;
-use std::rc::Rc;
+use std::{cell::RefCell, rc::Rc};
 
-#[derive(Clone)]
-pub struct Formiga<'a> {
-    ambiente: Rc<RefCell<Matriz<Formiga<'a>>>>,
-    raio_de_visao: u16,
+use rand::Rng;
+
+pub struct Formiga {
+    raio_de_visao: i32,
     pub posicao_x: i32,
     pub posicao_y: i32,
     pub estado: bool,
 }
 
-impl<'a> Formiga<'a> {
-    pub fn new(
-        ambiente: Rc<RefCell<Matriz<Formiga<'a>>>>,
-        raio_de_visao: u16,
-        posicao_x: i32,
-        posicao_y: i32,
-    ) -> Formiga<'a> {
-        Formiga {
-            ambiente: ambiente,
+impl Formiga {
+    pub fn new(raio_de_visao: i32, posicao_x: i32, posicao_y: i32) -> Self {
+        Self {
             raio_de_visao: raio_de_visao,
             posicao_x: posicao_x,
             posicao_y: posicao_y,
@@ -28,27 +19,31 @@ impl<'a> Formiga<'a> {
         }
     }
 
-    pub fn iteracao(&mut self) {
-        let ambiente = self.ambiente.borrow();
-
-        let (corpo, _) = ambiente.data[self.posicao_x as usize][self.posicao_y as usize];
-
-        drop(ambiente);
+    pub fn iteracao(
+        &mut self,
+        ambiente: &mut Vec<Vec<(bool, Option<Rc<RefCell<Formiga>>>)>>,
+        width: i32,
+        height: i32,
+    ) {
+        let (corpo, _) = ambiente[self.posicao_x as usize][self.posicao_y as usize];
 
         if !self.estado && corpo {
-            self.pegar();
+            self.pegar(ambiente, width, height);
         }
 
         if self.estado && !corpo {
-            self.soltar();
+            self.soltar(ambiente, width, height);
         }
 
-        self.andar();
+        self.andar(ambiente, width, height);
     }
 
-    fn porcentagem_soltar(&self) -> f64 {
-        let ambiente = self.ambiente.borrow();
-
+    fn porcentagem_soltar(
+        &self,
+        ambiente: &mut Vec<Vec<(bool, Option<Rc<RefCell<Formiga>>>)>>,
+        width: i32,
+        height: i32,
+    ) -> f64 {
         let mut itens_redor: i32 = 0;
 
         let raio_32 = self.raio_de_visao as i32;
@@ -59,25 +54,25 @@ impl<'a> Formiga<'a> {
             let mut indice_x = i;
 
             if i < 0 {
-                indice_x = ambiente.width + i;
+                indice_x = width + i;
             }
 
-            if i >= ambiente.width {
-                indice_x = i - ambiente.width;
+            if i >= width {
+                indice_x = i - width;
             }
 
             for j in self.posicao_y - raio_32..=self.posicao_y + raio_32 {
                 let mut indice_y = j;
 
                 if j < 0 {
-                    indice_y = ambiente.height + j;
+                    indice_y = height + j;
                 }
 
-                if j >= ambiente.width {
-                    indice_y = j - ambiente.height;
+                if j >= width {
+                    indice_y = j - height;
                 }
 
-                let (corpo, _) = ambiente.data[indice_x as usize][indice_y as usize];
+                let (corpo, _) = ambiente[indice_x as usize][indice_y as usize];
 
                 if corpo {
                     itens_redor += 1;
@@ -88,64 +83,13 @@ impl<'a> Formiga<'a> {
         itens_redor as f64 / celulas_redor as f64
     }
 
-    fn andar(&mut self) {
-        let mut sem_formiga: Vec<(i32, i32)> = Vec::new();
-
-        let mut ambiente = self.ambiente.borrow_mut();
-
-        for i in self.posicao_x - 1..=self.posicao_x + 1 {
-            let mut indice_x = i;
-
-            if i < 0 {
-                indice_x = ambiente.width + i;
-            }
-
-            if i >= ambiente.width {
-                indice_x = i - ambiente.width;
-            }
-
-            for j in self.posicao_y - 1..=self.posicao_y + 1 {
-                let mut indice_y = j;
-
-                if j < 0 {
-                    indice_y = ambiente.height + j;
-                }
-
-                if j >= ambiente.width {
-                    indice_y = j - ambiente.height;
-                }
-
-                let (_, formiga) = &ambiente.data[indice_x as usize][indice_y as usize];
-
-                if formiga.is_none() {
-                    sem_formiga.push((indice_x, indice_y));
-                }
-            }
-        }
-
-        if sem_formiga.is_empty() {
-            return;
-        }
-
-        let mut rng = rand::thread_rng();
-
-        let (x, y) = sem_formiga[rng.gen_range(0..sem_formiga.len())];
-
-        let (corpo, _) = ambiente.data[x as usize][y as usize];
-
-        let (corpo_atual, formiga_atual) =
-            ambiente.data[self.posicao_x as usize][self.posicao_y as usize].clone();
-
-        ambiente.data[x as usize][y as usize] = (corpo, formiga_atual);
-
-        ambiente.data[self.posicao_x as usize][self.posicao_y as usize] = (corpo_atual, None);
-
-        self.posicao_x = x;
-        self.posicao_y = y;
-    }
-
-    fn pegar(&mut self) {
-        let porcentagem_pegar = 1.0 - self.porcentagem_soltar();
+    fn pegar(
+        &mut self,
+        ambiente: &mut Vec<Vec<(bool, Option<Rc<RefCell<Formiga>>>)>>,
+        width: i32,
+        height: i32,
+    ) {
+        let porcentagem_pegar = 1.0 - self.porcentagem_soltar(ambiente, width, height);
 
         if porcentagem_pegar == 0.0 {
             return;
@@ -161,13 +105,16 @@ impl<'a> Formiga<'a> {
 
         self.estado = true;
 
-        let mut ambiente = self.ambiente.borrow_mut();
-        
-        ambiente.data[self.posicao_x as usize][self.posicao_y as usize].0 = false;
+        ambiente[self.posicao_x as usize][self.posicao_y as usize].0 = false;
     }
 
-    fn soltar(&mut self) {
-        let porcentagem_soltar = self.porcentagem_soltar();
+    fn soltar(
+        &mut self,
+        ambiente: &mut Vec<Vec<(bool, Option<Rc<RefCell<Formiga>>>)>>,
+        width: i32,
+        height: i32,
+    ) {
+        let porcentagem_soltar = self.porcentagem_soltar(ambiente, width, height);
 
         if porcentagem_soltar == 0.0 {
             return;
@@ -180,11 +127,68 @@ impl<'a> Formiga<'a> {
         if numero_aleatorio > porcentagem_soltar {
             return;
         }
-        
+
         self.estado = false;
 
-        let mut ambiente = self.ambiente.borrow_mut();
-        
-        ambiente.data[self.posicao_x as usize][self.posicao_y as usize].0 = true;
+        ambiente[self.posicao_x as usize][self.posicao_y as usize].0 = true;
+    }
+
+    fn andar(
+        &mut self,
+        ambiente: &mut Vec<Vec<(bool, Option<Rc<RefCell<Formiga>>>)>>,
+        width: i32,
+        height: i32,
+    ) {
+        let mut sem_formiga: Vec<(i32, i32)> = Vec::new();
+
+        for i in self.posicao_x - 1..=self.posicao_x + 1 {
+            let mut indice_x = i;
+
+            if i < 0 {
+                indice_x = width + i;
+            }
+
+            if i >= width {
+                indice_x = i - width;
+            }
+
+            for j in self.posicao_y - 1..=self.posicao_y + 1 {
+                let mut indice_y = j;
+
+                if j < 0 {
+                    indice_y = height + j;
+                }
+
+                if j >= width {
+                    indice_y = j - height;
+                }
+
+                let formiga = &ambiente[indice_x as usize][indice_y as usize].1;
+
+                if formiga.is_none() {
+                    sem_formiga.push((indice_x, indice_y));
+                }
+            }
+        }
+
+        if sem_formiga.is_empty() {
+            return;
+        }
+
+        let mut rng = rand::thread_rng();
+
+        let (x, y) = sem_formiga[rng.gen_range(0..sem_formiga.len())];
+
+        let (corpo, _) = ambiente[x as usize][y as usize];
+
+        let (corpo_atual, formiga_atual) =
+            ambiente[self.posicao_x as usize][self.posicao_y as usize].clone();
+
+        ambiente[x as usize][y as usize] = (corpo, formiga_atual);
+
+        ambiente[self.posicao_x as usize][self.posicao_y as usize] = (corpo_atual, None);
+
+        self.posicao_x = x;
+        self.posicao_y = y;
     }
 }
--- formigas/src/main.rs
+++ formigas/src/main.rs
@@ -1,169 +1,90 @@
 mod formiga;
-mod matriz;
+mod interface_grafica;
 
 use formiga::Formiga;
-use matriz::Matriz;
-use minifb::{Window, WindowOptions};
+use interface_grafica::Interface;
 use rand::Rng;
-use std::cell::RefCell;
-use std::rc::Rc;
-use std::thread;
-use std::time::Duration;
+use std::{cell::RefCell, rc::Rc, thread, time::Duration};
 
-const WIDTH: i32 = 15;
-const HEIGHT: i32 = 15;
-const ESCALA: i32 = 30;
-const NUM_FORMIGAS: i32 = 15;
-const NUM_CORPOS: i32 = 60;
-const RAIO_DE_VISAO: u16 = 1;
-
-fn atualizar_buffer(
-    buffer: &mut Vec<Vec<u32>>,
-    data: &Vec<Vec<(bool, Option<Rc<RefCell<Formiga>>>)>>,
-    x: i32,
-    y: i32,
-) {
-    for i in x - 1..=x + 1 {
-        let mut indice_x = i;
-
-        if i < 0 {
-            indice_x = WIDTH + i;
-        }
-
-        if i >= WIDTH {
-            indice_x = i - WIDTH;
-        }
-        for j in y - 1..=y + 1 {
-            let mut indice_y = j;
-
-            if j < 0 {
-                indice_y = HEIGHT + j;
-            }
-
-            if j >= HEIGHT {
-                indice_y = j - HEIGHT;
-            }
-
-            let (corpo, formiga) = &data[indice_x as usize][indice_y as usize];
-
-            if *corpo {
-                match formiga {
-                    Some(valor) => {
-                        if valor.borrow().estado {
-                            buffer[indice_x as usize][indice_y as usize] = 0xFF0000;
-                        } else {
-                            buffer[indice_x as usize][indice_y as usize] = 0x00FF00;
-                        }
-                    }
-                    None => {
-                        buffer[indice_x as usize][indice_y as usize] = 0x000000;
-                    }
-                }
-            } else {
-                match formiga {
-                    Some(valor) => {
-                        if valor.borrow().estado {
-                            buffer[indice_x as usize][indice_y as usize] = 0xFFFF00;
-                        } else {
-                            buffer[indice_x as usize][indice_y as usize] = 0x8B4513;
-                        }
-                    }
-                    None => {
-                        buffer[indice_x as usize][indice_y as usize] = 0xFFFFFF;
-                    }
-                }
-            }
-        }
-    }
-}
-
-pub fn display(window: &mut Window, buffer: &Vec<Vec<u32>>) {
-    let flat_buffer: Vec<u32> = buffer.iter().flat_map(|row| row.iter()).cloned().collect();
-    window
-        .update_with_buffer(&flat_buffer, WIDTH as usize, HEIGHT as usize)
-        .unwrap();
-}
+const WIDTH: i32 = 40;
+const HEIGHT: i32 = 40;
+const ESCALA: i32 = 20;
+const NUM_FORMIGAS: i32 = 40;
+const NUM_CORPOS: i32 = 100;
+const RAIO_DE_VISAO: i32 = 1;
 
 fn main() {
-    let matriz: Rc<RefCell<Matriz<Formiga>>> = Rc::new(RefCell::new(Matriz::new(WIDTH, HEIGHT)));
+    let mut ambiente: Vec<Vec<(bool, Option<Rc<RefCell<Formiga>>>)>> =
+        vec![vec![(false, None); WIDTH as usize]; HEIGHT as usize];
     let mut formigas: Vec<Rc<RefCell<Formiga>>> = Vec::with_capacity(NUM_FORMIGAS as usize);
-
-    let mut buffer: Vec<Vec<u32>> = vec![vec![0xFFFFFF; WIDTH as usize]; HEIGHT as usize];
-
-    let mut window = Window::new(
-        "Formigas!",
-        (WIDTH * ESCALA) as usize,
-        (HEIGHT * ESCALA) as usize,
-        WindowOptions::default(),
-    )
-    .unwrap();
+    let mut interface = Interface::new(WIDTH, HEIGHT, ESCALA);
 
     let mut rng = rand::thread_rng();
 
-    let mut ambiente = matriz.borrow_mut();
-
-    // Criar formigas
     for _ in 0..NUM_FORMIGAS {
         loop {
             let posicao_x = rng.gen_range(0..WIDTH);
             let posicao_y = rng.gen_range(0..HEIGHT);
 
-            let (_, formiga) = &ambiente.data[posicao_x as usize][posicao_y as usize];
+            let formiga = &ambiente[posicao_x as usize][posicao_y as usize].1;
 
             if formiga.is_none() {
                 let nova_formiga = Rc::new(RefCell::new(Formiga::new(
-                    Rc::clone(&matriz),
                     RAIO_DE_VISAO,
                     posicao_x,
                     posicao_y,
                 )));
-                formigas.push(Rc::clone(&nova_formiga));
-                ambiente.data[posicao_x as usize][posicao_y as usize].1 =
-                    Some(Rc::clone(&nova_formiga));
-                atualizar_buffer(&mut buffer, &ambiente.data, posicao_x, posicao_y);
+                formigas.push(nova_formiga.clone());
+                ambiente[posicao_x as usize][posicao_y as usize].1 = Some(nova_formiga);
+                interface.atualizar_buffer(&ambiente, posicao_x, posicao_y);
                 break;
             }
         }
     }
 
-    // Criar corpos
     for _ in 0..NUM_CORPOS {
         loop {
             let posicao_x = rng.gen_range(0..WIDTH);
             let posicao_y = rng.gen_range(0..HEIGHT);
 
-            let (corpo, _) = ambiente.data[posicao_x as usize][posicao_y as usize];
+            let (corpo, _) = ambiente[posicao_x as usize][posicao_y as usize];
 
             if !corpo {
-                ambiente.data[posicao_x as usize][posicao_y as usize].0 = true;
-                atualizar_buffer(&mut buffer, &ambiente.data, posicao_x, posicao_y);
+                ambiente[posicao_x as usize][posicao_y as usize].0 = true;
+                interface.atualizar_buffer(&ambiente, posicao_x, posicao_y);
                 break;
             }
         }
     }
 
-    drop(ambiente);
-
-    let mut rodar = true;
-
-    while window.is_open() {
-        thread::sleep(Duration::from_secs(1));
-        display(&mut window, &buffer);
-        if rodar {
-            thread::sleep(Duration::from_secs(5));
-            for _ in 0..10000 {
-                for i in 0..NUM_FORMIGAS {
-                    let mut formiga = formigas[i as usize].borrow_mut();
-                    let x = formiga.posicao_x;
-                    let y = formiga.posicao_y;
-                    formiga.iteracao();
-                    drop(formiga);
-                    atualizar_buffer(&mut buffer, &matriz.borrow().data, x, y);
-                }
-                display(&mut window, &buffer);
-            }
+    thread::sleep(Duration::from_secs(1));
+    interface.display();
+    thread::sleep(Duration::from_secs(5));
+    for _ in 0..20000 {
+        for i in 0..NUM_FORMIGAS {
+            let mut formiga = formigas[i as usize].borrow_mut();
+            formiga.iteracao(&mut ambiente, WIDTH, HEIGHT);
+            let x = formiga.posicao_x;
+            let y = formiga.posicao_y;
+            drop(formiga);
+            interface.atualizar_buffer(&ambiente, x, y);
         }
-        rodar = false;
-        display(&mut window, &buffer);
+        interface.display();
     }
+    for i in 0..NUM_FORMIGAS {
+        loop {
+            let mut formiga = formigas[i as usize].borrow_mut();
+            if !formiga.estado {
+                break;
+            }
+            formiga.iteracao(&mut ambiente, WIDTH, HEIGHT);
+            let x = formiga.posicao_x;
+            let y = formiga.posicao_y;
+            drop(formiga);
+            interface.atualizar_buffer(&ambiente, x, y);
+        }
+    }
+    loop{    interface.display();}
+
+    
 }
