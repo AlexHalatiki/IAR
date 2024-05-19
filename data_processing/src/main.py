d9a7ebc1b0d67f342b8285c2e1bd3f4dea0906ab
alexhalatiki <alexhvicente1@gmail.com>
2024-05-19 17:22:15 -0300

    Algumas alteracoes

--- data_processing/src/main.py
+++ data_processing/src/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 
+# Funcao para aplicar One Hot Encoding 
 def one_hot_encoding(coluna_especifica, df):
     encoder = OneHotEncoder(sparse_output=False)
     
@@ -17,12 +18,14 @@ def one_hot_encoding(coluna_especifica, df):
 
     return pd.concat([df, one_hot_df], axis=1)
     
-
+# Lendo base de dados
 dados = pd.read_csv('./data/Airbnb_Data.csv')
 
-dados.drop(columns=['NAME', 'country', 'host name', 'last review', 'license'], inplace=True)
+# Excluindo atributos irrelevantes
+dados.drop(columns=['id', 'NAME', 'host id', 'host name', 'neighbourhood', 'country', 'country code', 'host name', 'last review', 'house_rules', 'license'], inplace=True)
 
-dados.dropna(subset=['host_identity_verified', 'neighbourhood group', 'neighbourhood', 'lat', 'long', 'instant_bookable', 'cancellation_policy', 'room type', 'Construction year', 'number of reviews', 'reviews per month', 'review rate number', 'price'], inplace=True)
+# Excluindo e tratando valores nulos
+dados.dropna(subset=['host_identity_verified', 'neighbourhood group', 'lat', 'long', 'instant_bookable', 'cancellation_policy', 'room type', 'Construction year', 'number of reviews', 'reviews per month', 'review rate number', 'price'], inplace=True)
 
 dados['Construction year'] = dados['Construction year'].astype(int)
 dados['number of reviews'] = dados['number of reviews'].astype(int)
@@ -38,15 +41,27 @@ dados = dados[dados['availability 365'] >= 0]
 
 dados['service fee'] = dados['service fee'].fillna(0).astype(int)
 
+# Padronizacao
 corrections = {
     'brookln': 'Brooklyn',
 }
 
 dados['neighbourhood group'] = dados['neighbourhood group'].replace(corrections)
 
+# Aplicando One Hot Encoding
+dados = one_hot_encoding('host_identity_verified', dados)
+dados = one_hot_encoding('neighbourhood group', dados)
+dados = one_hot_encoding('instant_bookable', dados)
 dados = one_hot_encoding('cancellation_policy', dados)
 dados = one_hot_encoding('room type', dados)
 
+# Tratando "Outliers"
 dados['price'] = dados['price'].apply(lambda x: (x + "0").replace(',', '') if ',' in x and len(x) == 4 else x.replace(',', ''))
+dados['long'] = dados['long'].astype(str).apply(lambda x: x[:3] + '.' + x[3:].replace('.', ''))
 
+# Movendo price pro final
+cols = [col for col in dados.columns if col != 'price'] + ['price']
+dados = dados[cols]
+
+# Salvando a base de dados processada
 dados.to_csv('./data/Airbnb_Data_Processed.csv', index=False)
